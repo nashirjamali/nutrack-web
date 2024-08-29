72eb2692cc4ed3b5f12bac7118fb3c38d43fa755
denandrapr <denandra1628@gmail.com>
2024-08-29 14:50:21 +0800

    fix tailwind material error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type {Config} from 'tailwindcss';
+import withMT from "@material-tailwind/react/utils/withMT"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/pages/*/.{js,ts,jsx,tsx,mdx}',
+    './src/components/*/.{js,ts,jsx,tsx,mdx}',
+    './src/app/*/.{js,ts,jsx,tsx,mdx}'
   ],
-  plugins: [require('@tailwindcss/forms')]
 };
-export default config;
+
+export default withMT(config);
