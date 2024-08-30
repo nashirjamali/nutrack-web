a6a40b602b73485d41ecae690e16fd3a7ae6ac30
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-08-30 23:40:40 +0700

    add loading

--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -24,6 +24,7 @@ export default function InputScreen() {
   const [lunchValue, setLunchValue] = useState('');
   const [dinnerValue, setDinnerValue] = useState('');
   const [snackValue, setSnackValue] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleBreakfastChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -46,6 +47,8 @@ export default function InputScreen() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    setLoading(true);
+
     await submit(
       {
         breakfast: breakfastValue,
@@ -57,6 +60,7 @@ export default function InputScreen() {
     );
 
     router.back();
+    setLoading(false);
   };
 
   return (
@@ -108,7 +112,13 @@ export default function InputScreen() {
               />
             </CardBody>
             <CardFooter className="pt-0 mt-8">
-              <Button type="submit" variant="filled" color="green" fullWidth>
+              <Button
+                type="submit"
+                variant="filled"
+                color="green"
+                fullWidth
+                loading={loading}
+              >
                 Submit
               </Button>
             </CardFooter>
