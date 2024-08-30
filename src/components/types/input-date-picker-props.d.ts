96d667e928f30e7bbd22d102d9c1c777d1613d24
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-08-30 23:26:43 +0700

    delete unused components

--- src/components/types/input-date-picker-props.d.ts
+++
@@ -1,7 +0,0 @@
-import { InputProps } from "@material-tailwind/react";
-import { DateValueType } from "react-tailwindcss-datepicker";
-
-export interface InputDatePickerProps extends InputProps {
-  value: DateValueType;
-  onChange: (value: DateValueType) => void;
-}
